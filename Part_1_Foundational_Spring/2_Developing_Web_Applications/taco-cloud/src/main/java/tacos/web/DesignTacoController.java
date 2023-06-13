19419994a1968ee96f567b07f3416df8ccf76dff
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-13 11:12:57 -0500

    Starting to work on validating formating input

--- Part_1_Foundational_Spring/2_Developing_Web_Applications/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ Part_1_Foundational_Spring/2_Developing_Web_Applications/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -4,10 +4,7 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 
 import java.util.Arrays;
@@ -59,6 +56,14 @@ public class DesignTacoController {
     public String showDesignForm(){
         return "design";
     }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}",taco);
+        return "redirect:/orders/current";
+    }
+
     private Iterable<Ingredient> filterByType(
             List<Ingredient> ingredients, Type type) {
         return ingredients
